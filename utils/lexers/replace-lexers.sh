7fcc14597b5f8cf765bdd7286c4d6306172f6c0d
Allan Leal <allanmulin@gmail.com>
2022-01-04 17:18:42 +0100

    Improved python/cpp pygments lexers

---
+++ utils/lexers/replace-lexers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+script_path=$(realpath "$0")
+script_dir=$(dirname $script_path)
+
+pygments_init_file=$(python -c "import pygments; print(pygments.__file__)")
+pygments_dir=$(dirname $pygments_init_file)
+
+echo $script_path
+echo $pygments_dir
+
+cp $pygments_dir/lexers/python.py $pygments_dir/lexers/python-bkp.py
+cp $pygments_dir/lexers/c_cpp.py $pygments_dir/lexers/c_cpp-bkp.py
+
+cp $script_dir/python.py $pygments_dir/lexers
+cp $script_dir/c_cpp.py $pygments_dir/lexers
