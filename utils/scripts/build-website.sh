736b396ad4accbaf8e92d62900472d56758d0496
Allan Leal <allanmulin@gmail.com>
2022-01-06 16:11:02 +0100

    Implemented build-website.sh script

---
+++ utils/scripts/build-website.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e  # exit when any command fails
+
+script_path=$(realpath "$0")
+script_dir=$(dirname $script_path)
+root_dir=$script_dir/../../
+build_dir=$root_dir/_build
+html_dir=$build_dir/html
+
+# Needed for the conda activate commands below to work (reference: https://github.com/conda/conda/issues/7980#issuecomment-492784093)
+eval "$(conda shell.bash hook)"
+
+# =============================================================================
+# Build the Jupyter Book documentation
+# =============================================================================
+conda activate reaktoro-jupyter-book
+
+jupyter-book build $root_dir
+
+# =============================================================================
+# Build Reaktoro v1 website (documentation + API reference)
+# =============================================================================
+
+rm -rf $build_dir/v1
+mkdir $build_dir/v1
+cd $build_dir/v1
+
+wget https://github.com/reaktoro/reaktoro/archive/refs/heads/v1.zip -O v1.zip
+unzip -o -qq v1.zip
+
+cd reaktoro-1
+mkdir build
+cd build
+
+conda activate reaktoro-v1
+cmake ..
+make website
+cp -r docs/website $html_dir/v1
+
+# =============================================================================
+# Build Reaktoro v2 API reference
+# =============================================================================
+
+rm -rf $build_dir/v2
+mkdir $build_dir/v2
+cd $build_dir/v2
+
+wget https://github.com/reaktoro/reaktoro/archive/refs/heads/main.zip -O main.zip
+unzip -o -qq main.zip
+
+cd reaktoro-main
+mkdir build
+cd build
+
+conda activate reaktoro
+cmake ..
+make docs
+cp -r docs/html $html_dir/api
