0d39a078b4471878d09a04c731e87d70ed9bd579
Allan Leal <allanmulin@gmail.com>
2022-11-18 15:30:55 +0100

    External links are now indicated and opened in new tab

--- _static/customize.js
+++ _static/customize.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-    $(".navbar_extra_footer").html('');
+    $(".navbar_extra_footer").html(""); // clear extra footer
+    $("a.reference.external").attr("target", "_blank"); // external links open in a new tab
 });
